405e10652011956427ff4c7718b30270e9046a4e
Andrei Borza <andrei.borza@gmail.com>
2016-04-12 00:50:53 +0900

    T U R N !

---
+++ run.py
@@ -0,0 +1,21 @@
+import os
+import tornado.ioloop
+import tornado.web
+
+js_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "js"),
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+def run():
+  return tornado.web.Application([
+    (r"/js/(.*)", tornado.web.StaticFileHandler, {"path": "./js"}),
+    (r"/textures/(.*)", tornado.web.StaticFileHandler, {"path": "./textures"}),
+    (r"/", MainHandler),
+  ], debug=True)
+
+if __name__ == "__main__":
+  app = run()
+  app.listen(1337)
+  tornado.ioloop.IOLoop.current().start()
